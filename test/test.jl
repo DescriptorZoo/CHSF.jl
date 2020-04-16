e4cd9bc9e3cec24074d81048f80a2584f51aa159
Berk Onat <onatberk@MacBerk-Pro.local>
2020-04-16 22:25:57 +0200

    Reduce test Si size.

--- test/test.jl
+++ test/test.jl
@@ -4,7 +4,7 @@
 @info("Testing CHSF Descriptor for DC 4x4x4 Si with cutoff=6.5, n=2, l=2")
 using CHSF, JuLIP, Test
 
-at = bulk(:Si, cubic=true) * (4,4,4)
+at = bulk(:Si, cubic=true)
 desc = chsf(at, 6.5, n=2, l=2)
 #round.(desc[1], digit=7) == chsfpy
 chsf_ref = [10.3698237,1.4503467,-8.2118063,51.6882200,-53.0113716,69.8233316] #n=2,l=2 case
